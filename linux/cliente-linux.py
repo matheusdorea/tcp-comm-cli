c7629f81a3b18a35fa6c07df3089f778abf5a672
matheusdorea <matheusdorea33@gmail.com>
2026-03-18 22:00:05 -0300

    feat: utilizando a biblioteca curses para melhoria da experiência no terminal do cliente

--- linux/cliente-linux.py
+++ linux/cliente-linux.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import curses
 
 # 1. Cria socket TCP
 cliente = socket.socket(
@@ -16,38 +17,78 @@ lock = threading.Lock()
 # 2. Conecta ao servidor
 cliente.connect((HOST, PORT))
 
-def iniciar_cliente():
-    cliente.recv(BUFFERSIZE)
-    msg = input("Apelido: ")
-    cliente.send(msg.encode())
-    print(cliente.recv(BUFFERSIZE))
-    
-def enviar_mensagens():
+def main(stdscr = curses.initscr()):
     global rodando
-    while rodando:
-        msg = input("Você: ")
+    
+    curses.curs_set(1)
+    stdscr.clear()
+    
+    altura, largura = stdscr.getmaxyx()
+    
+    painel_msgs = curses.newwin(altura - 2, largura, 0, 0)
+    painel_msgs.scrollok(True)
+    
+    separador = curses.newwin(1, largura, altura-2, 0)
+    separador.addstr(0, 0, "-" * (largura - 1))
+    separador.refresh()
+    
+    painel_input = curses.newwin(1, largura, altura - 1, 0)
+    
+    def adicionar_mensagem(msg):
+        with lock:
+            painel_msgs.addstr(f"{msg}\n")
+            painel_msgs.refresh()
 
-        if msg == "/logout":
+            
+    prompt = cliente.recv(BUFFERSIZE).decode()
+    adicionar_mensagem(prompt)
+    
+    #iniciando cliente
+    painel_input.clear()
+    painel_input.addstr("Apelido: ")
+    painel_input.refresh()
+    curses.echo()
+    apelido = painel_input.getstr().decode()
+    curses.noecho()
+    cliente.send(apelido.encode())
+    
+    boas_vindas = cliente.recv(BUFFERSIZE).decode()
+    adicionar_mensagem(boas_vindas)
+    
+    def receber_mensagens():
+        global rodando
+        while rodando:
+            try:
+                msg = cliente.recv(BUFFERSIZE).decode()
+                if msg:
+                    adicionar_mensagem(msg)
+            except:
+                break
+            
+    thread_receber = threading.Thread(target=receber_mensagens)
+    thread_receber.start()
+    
+    while rodando:
+        painel_input.clear()
+        painel_input.addstr("Você: ")
+        painel_input.refresh()
+        
+        curses.echo()
+        try:
+            msg = painel_input.getstr().decode()
+        except:
+            pass
+        curses.noecho()
+        
+        if msg == "/sair":
             with lock:
                 rodando = False
             break
-
+        
         cliente.send(msg.encode())
         
     cliente.close()
-    
-def receber_mensagens():
-    global rodando
-    while rodando:
-        print(cliente.recv(BUFFERSIZE).decode())
-
-thread_enviar = threading.Thread(target=enviar_mensagens)
-thread_receber = threading.Thread(target=receber_mensagens)
-
-thread_enviar.start()
-thread_receber.start()
-
-thread_enviar.join()
-thread_receber.join()
+                
+curses.wrapper(main)
 
 print("Cliente encerrado...")
--- linux/servidor-linux.py
+++ linux/servidor-linux.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import curses
 
 
 servidor = socket.socket(
